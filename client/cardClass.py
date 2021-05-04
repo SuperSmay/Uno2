ec31427a3d0670cf60666501cfd131ce530a3e16
Smay <samaskling@gmail.com>
2021-05-03 21:03:11 -0500

    Code commenting

--- client/cardClass.py
+++ client/cardClass.py
@@ -1,38 +1,32 @@
 import storage.cardDictionaries as cardDictionaries
 
 class Card:
-    def __init__(self, color, face, isColorChoice = False, returnable = True):
-        self.isColorChoice = isColorChoice
-        self.color = color
-        self.returnable = returnable
-        self.face = face
-        self.image = cardDictionaries.cardImages[str(face) + "_" + color]
-        self.emoji = cardDictionaries.cardEmoji[str(face) + "_" + color]
-        self.colorCode = self.getColorCode()
+    def __init__(self, color: str, face, returnable: bool = True):
+        '''
+        A card object for Uno.\n
+        Attributes:
+            - color: str; The color of the card
+            - returnable: bool; Whether or not the card should be put back in the deck when played
+            - face: str or int; The number or type of the card
+            - image: str; A link to the image for the card
+            - emoji: str; The emoji code for the card
+        '''
+        self.color = color  #The color of the card stored as a string.
+        self.returnable = returnable  #Boolean used to define if the card is one that should be returned the the deck. Default true.
+        self.face = face  #The face of the card, either its number or the name of its type. Ex: 1, 7, "skip", "plus4"
+        self.image = cardDictionaries.cardImages[str(face) + "_" + color]  #Gets the image for the card as a link
+        self.emoji = cardDictionaries.cardEmoji[str(face) + "_" + color]  #Gets the emojo tag for the card
+        self.colorCode = self._getColorCode()  #Gets the color code of the card, used for embed colors
 
-    def getColorCode(self):
+    def _getColorCode(self) -> int:
+        '''
+        Returns: int; The color code for the this card
+        '''
         if self.color == "blue": return 29372               
         elif self.color == "green": return 5876292
         elif self.color == "yellow": return 16768534
         elif self.color == "red": return 15539236
         else: return 4802889
 
-    def __str__(self):
+    def __str__(self) -> str:  #When this object is cast to a string, this is what it well show as
         return f"{self.color} {self.face}"
-
-class FakeCard:
-    def __init__(self, card):
-        self.isColorChoice = card.isColorChoice
-        self.color = card.color
-        self.returnable = False
-        self.face = card.face
-        self.image = cardDictionaries.cardImages[str(card.face) + "_" + card.color]
-        self.emoji = cardDictionaries.cardEmoji[str(card.face) + "_" + card.color]
-        self.colorCode = self.getColorCode()
-
-    def getColorCode(self):
-        if self.color == "blue": return 29372               
-        elif self.color == "green": return 5876292
-        elif self.color == "yellow": return 16768534
-        elif self.color == "red": return 15539236
-        else: return 4802889
--- client/commands.py
+++ client/commands.py
@@ -1,19 +1,39 @@
 import discord
-from storage.globalVariables import openGames, openLobbies, playersInGame, playersInLobby, channelInGame, channelHasLobby, playerInGame, playerInLobby, client
+from storage.globalVariables import channelHasLobby, channelInGame, client, openGames, openLobbies, playerInGame, playerInLobby, playersInGame, playersInLobby
+
+from client.game import Game, GameLobby
 from client.messageClasses import HelpMessage
-from client.game import GameLobby, Game
-    
-async def fooTest(ctx):
+
+### Commands
+
+async def fooTest(ctx) -> None:  #Foo command
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+    Sends ">>>bar" to the ctx object given.\n
+    Returns: None
+    '''
     await ctx.send(">>> bar")
 
-async def help(ctx, argCommand):  #Help command
+async def help(ctx, argCommand: str) -> None:  #Help command
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+        - argCommand: str; The name of a command to get help for.\n
+    Creates and sends an interactble help message as a response to being called. argCommand must be a valid choice from the list ["main", "join", "leave", "start", "rules", "ping"] or an exception will be thrown.\n
+    Returns: None
+    '''
     helpMessageObject = HelpMessage(ctx, argCommand)  #Creates a new help object for the message
     await helpMessageObject.sendMessage(ctx)  #Sends the message from the object
 
-async def ping(ctx, latency):
-    await ctx.send(f"Pong! {round(latency*1000, 2)}ms")  #Calculates the client ping     
-
-async def join(ctx):
+async def join(ctx) -> None:  #Join command
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+    Attempts to join the player to the lobby in the channel the command was called in.\n
+    If it succeeds, the player is added to the lobby and a success message is sent to the channel. If it fails, nothing happens and the reason is sent to the channel.\n
+    Returns: None
+    '''
     if channelInGame(ctx.channel.id):
         await ctx.send("The game in this channel is already in progress. Switch to a different channel or wait for this game to end.")
         return 
@@ -28,7 +48,15 @@ async def join(ctx):
     openLobbies[ctx.channel.id].playerJoin(ctx.author.id)  #Gets the lobby object from the dictionary and calls playerJoin with the author ID
     await ctx.send(f"{ctx.author.name} has joined the lobby. {lobbyCount(ctx.channel.id)}")  #Send join message
 
-async def leave(ctx):
+async def leave(ctx) -> None:
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+    Attempts to remove the player from any active games or lobbies they may be in.\n
+    Responds with a single leave message if the command is used in the channel of the game/lobby.\n
+    Responds in both the channel of the game/lobby and the channel where the command was used if the command is used in a channel different from the one of the game/lobby.\n
+    Returns: None
+    '''
     if playerInLobby(ctx.author.id):  #If player in lobby
         if playersInLobby[ctx.author.id] == ctx.channel.id:  #If the channelID the message was sent in matches the ID of the lobby
             await ctx.send(f"You left the lobby. {lobbyCount(ctx.channel.id, - 1)}")  #Prints lobby leave message and the count message minus 1. Count is minus one because it runs before the user is actually removed
@@ -46,24 +74,43 @@ async def leave(ctx):
     else:
         ctx.send("You are not in a lobby or game.")
 
-async def lobby(ctx):
+async def lobby(ctx) -> None:
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+    Responds with an embed representing the lobby in the channel the command was called in.\n
+    Returns: None
+    '''
     lobbyEmbed=discord.Embed(title="**Uno2 Lobby**", description=f"This is the lobby for <#{ctx.channel.id}>.", color=0xe32c22)  
     if channelHasLobby(ctx.channel.id):  #If there is a lobby, get all the players in it and list them. If not, then say the lobby is empty
         for playerID in openLobbies[ctx.channel.id].players:
             player = await client.fetch_user(playerID)
             lobbyEmbed.add_field(name=f"{player.name}", value="Work in progress", inline=True)
-        lobbyEmbed.set_footer(text= f"{lobbyCount(ctx.channel.id).replace('now ', '')}")  #Uses the same function to get the count text, but removes the word "now"
+        lobbyEmbed.set_footer(text= f"{lobbyCount(ctx.channel.id).replace('now ', '')}")  #Uses the same function to get the count text as the leave command, but removes the word "now"
+    elif channelInGame(ctx.channel.id):  #If the game has started, just leave the lobby embed empty TODO - Fix this to show the current game stats
+        lobbyEmbed.add_field(name=f"The game in this channel has already started", value="Work in progress", inline=True)
     else:
         lobbyEmbed.add_field(name=f"Lobby is empty.", value=f"Type `/join` to join.", inline=True)
     await ctx.send(embed=lobbyEmbed)
 
-def lobbyCount(channelID, offset=0):  #Returns a formatted message of the amount of players in the lobby. "There are now __ player(s) in the lobby". Offset is for leave messages mostly
-    if len(openLobbies[channelID].players) + offset == 1:
-        return f"There is now 1 player in the lobby."
-    else:
-        return f"There are now {len(openLobbies[channelID].players) + offset} players in the lobby."
-
-async def start(ctx):  #Starts the game in the current channel
+async def ping(ctx) -> None:  #Ping command
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+    Sends "Pong! (latency)ms" as a response to being called.\n
+    Returns: None
+    '''
+    await ctx.send(f"Pong! {round(client.latency*1000, 2)}ms")  #Calculates then sends the ping message with the client latency, rounding it to two decimal points
+    
+async def start(ctx) -> None:  #Starts the game in the current channel
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+    Attempts to start the game for the user of the command in the channel the command was called in.\n
+    If the game cannot be started, the reason why is sent the channel.\n
+    If the game can be started, the game object is created from the lobby in the channel the command was called from, and the lobby is closed.\n
+    Returns: None
+    '''
     if channelInGame(ctx.channel.id):
         await ctx.send("The game in this channel is already in progress. Switch to a different channel or wait for this game to end.")
         return 
@@ -83,7 +130,21 @@ async def start(ctx):  #Starts the game in the current channel
         await ctx.send(f"There are not enough players in this lobby. Get at least one more to join with `/join`.")
         return
     else:
-        game = Game(openLobbies[ctx.channel.id])
-        openGames[ctx.channel.id] = game
-        await game.startGame()
-        del(openLobbies[ctx.channel.id])
+        game = Game(openLobbies[ctx.channel.id])  #Create new game object
+        await game.startGame()  #Starts the newly created game
+        del(openLobbies[ctx.channel.id])  #Deletes the lobby from the dictionary
+
+### Other
+
+def lobbyCount(channelID, offset: int = 0) -> str:  #Returns a formatted message of the amount of players in the lobby. "There are now __ player(s) in the lobby". Offset is for leave messages mostly
+    '''
+    Parameters:
+        - ctx: Discord context object for the command call.\n
+        - offset: int; Offset for the count in the returned message\n
+    Returns: str; A short message about the lobby player count. 
+    '''
+    if len(openLobbies[channelID].players) + offset == 1:
+        return f"There is now 1 player in the lobby."
+    else:
+        return f"There are now {len(openLobbies[channelID].players) + offset} players in the lobby."
+        
